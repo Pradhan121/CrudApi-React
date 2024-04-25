0e2491934709e196f45ed502a4570fac572def2d
pradhan121 <sm.pradhan101.gmail.com>
2024-04-25 17:42:51 +0530

    chnge

---
+++ src/component1/EditForm.js
@@ -0,0 +1,172 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { Button } from "@mui/material";
+
+
+const EditForm = () => {
+    const data = {
+        customerType: "",
+        customerName: "",
+        customerMobile: "",
+        customerEmail: "",
+        customerNo: "",
+        birthday: "",
+        healthInsurance: "",
+    };
+
+    const [inputdata, setInputdata] = useState(data);
+
+    const handleInput = (e) => {
+        setInputdata({ ...inputdata, [e.target.name]: e.target.value });
+    };
+      let {id}=useParams();
+      
+      useEffect(() => {
+        axios.get("https://hammerhead-app-tzlph.ondigitalocean.app/customers/" + id , inputdata)
+        .then((resp) =>setInputdata(resp.data))
+       .catch((err) => console.log(err));
+        },[])
+     //console.log("id===>" + id);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        axios
+            .put(
+                "https://hammerhead-app-tzlph.ondigitalocean.app/customers/"+ id , inputdata)
+            .then((resp) => {
+                console.log(resp);
+                alert("Are you sure \n you want to update the customer");
+            })
+            .catch((err) => console.log(err));
+    }
+    // let { id } = useParams();
+
+    // console.log("id===>" + id);
+
+    return (
+        <>
+
+            <div className="app">
+                
+                <h2>Edit Customer</h2>
+                <form>
+                    <div>
+                        <label>customerType: </label>
+                        <input
+                            type="number"
+                            onChange={handleInput}
+                            id="number"
+                            name="customerType"
+                            min="0"
+                            max="50"
+                            autoComplete="off"
+                           value={inputdata.customerType}
+                            
+                        />
+                     </div>
+                         <br></br>
+                         
+                     <div className="container">
+                        <label> customerName:</label>
+                        <input
+                            type="text"
+                            onChange={handleInput}
+                            id="username"
+                            name="customerName"
+                            autoComplete="off"
+                            value={inputdata.customerName}
+                        />
+                     </div>
+                        <br></br>
+                       
+                     <div className="container-line" >
+                        <label> customerMobile:</label>
+                         <input
+                            type="tel"
+                            onChange={handleInput}
+                            id="mobileNo"
+                            name="customerMobile"
+                            autoComplete="off"
+                            value={inputdata.customerMobile}
+                         />
+                     </div>
+                        <br></br>
+                       
+                     <div className="show">
+                        <label> customerEmail:</label>
+                         <input
+                            type="email"
+                            onChange={handleInput}
+                            id="emailId"
+                            name="customerEmail"
+                            autoComplete="off"
+                            value={inputdata.customerEmail}
+                         />
+                     </div>
+                        <br></br>
+                       
+                        <div>
+                        <label> customerNo:</label>
+                         <input
+                            type="number"
+                            onChange={handleInput}
+                            id="counter"
+                            name="customerNo"
+                            min="0"
+                            max="50"
+                            autoComplete="off"
+                            value={inputdata.customerNo}
+                         />
+                     </div>
+                         <br></br>
+                        
+                     <div>
+                        <label> birthday:</label>
+                          <input
+                            type="date"
+                            onChange={handleInput}
+                            id="dateOfBirth"
+                            name="birthday"
+                            autoComplete="off"
+                            value={inputdata.birthday}
+                         />
+                     </div>
+                         <br></br>
+                        
+                     <div className="demo">
+                        <label> healthInsurance:</label>
+                          <input
+                            type="text"
+                            onChange={handleInput}
+                            id="number"
+                            name="healthInsurance"
+                            value={inputdata.healthInsurance}
+                          />
+                         <br></br>
+                         <br></br>
+                         
+                      <button
+                            onClick={handleSubmit}
+                             style={{
+                                 backgroundColor:"darkviolet",
+                                 color: "white",
+                                 marginBottom: "10px",
+                                 marginLeft: "30px",
+                                 borderRadius:"5px"
+                              }}
+                           
+                                 type="submit"
+                          >
+                            Update
+                         </button> 
+                         <button style={{borderRadius:"5px", color:"black",marginLeft:'50px'}}>
+                            Cancel
+                         </button>
+                    </div>
+                </form>
+            </div>
+        </>
+    );
+};
+export default EditForm;
--- src/component1/Form.js
+++ src/component1/Form.js
@@ -1,75 +1,150 @@
-import React, { useState } from 'react'
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
-const Form =() => {
+
+  const Form = () => {
     const data = {
-       customerType:"",customerName:"", customerMobile:'',
-       customerEmail:"",customerNo:'',Birthday:'',HealthInsurance:''
-         };
+        customerType: "",
+        customerName: "",
+        customerMobile: "",
+        customerEmail: "",
+        customerNo: "",
+        birthday: "",
+        healthInsurance: "",
+    };
 
-         const[inputdata, setInputdata] = useState(data);
+    const [inputdata, setInputdata] = useState(data);
 
-         const handleInput = (e) => {
-           setInputdata({...inputdata,[e.target.name]: e.target.value})
+    const handleInput = (e) => {
+        setInputdata({ ...inputdata, [e.target.name]: e.target.value });
+    };
 
-         }
+    function handleSubmit(e) {
+        e.preventDefault();
+        axios
+            .post(
+                "https://hammerhead-app-tzlph.ondigitalocean.app/customers",
+                inputdata
+            )
+            .then((resp) => {
+                console.log(resp.inputdata);
+                alert("Are you sure");
+            })
+         .catch((err) => console.log(err));
+    }
 
-         function handleSubmit(e) {
-          e.preventDefault();
-          axios.post('https://hammerhead-app-tzlph.ondigitalocean.app/customers',  inputdata )
-          .then((resp) => {console.log(resp.inputdata) })
-          .catch(err => console.log(err))
-         }
+    return (
+        <>
+           <div>
+            <div className=" w-50 border bg-white shadow px-5 pt-3 pb-5 rounded">
+                <h2>Add Customer</h2>
+                <form>
+                    <div>
+                        <label>customerType:
+                        <input
+                            type="number"
+                            onChange={handleInput}
+                            id="number"
+                            name="customerType"
+                            min="0"
+                            max="50"
+                            value={inputdata.customerType}
+                            autoComplete="off"
+                        />
+                        </label>
+                           <br></br>
+                        <label> customerName:
+                        <input
+                            type="text"
+                            onChange={handleInput}
+                            id="username"
+                            name="customerName"
+                            value={inputdata.customerName}
+                            autoComplete="off"
+                            placeholder="enter your name"
+                        />
+                        </label>
 
-    return(
-      <>
-         <div className='app'  >
-       <form >
-        <div>
-
-        <label>customerType: </label>
-           <input type="number" onChange={handleInput} id="number" name="customerType" min='0' max='50'  
-              autoComplete='off' />
-                 <br></br>
-                 <br></br>
-       <label> customerName:</label>
-        <input type="text"onChange={handleInput} id="username" name='customerName'
-            autoComplete='off' />
-            
-               <br></br>
-               <br></br>
-          <label> customerMobile:</label>
-          <input type="tel" onChange={handleInput} id="mobileNo" name='customerMobile'
-            autoComplete='off' />
-           
-              <br></br>
-              <br></br>
-         <label htmlFor="emailId"> customerEmail</label>
-          <input type="email"  onChange={handleInput} id="emailId" name='customerEmail'
-            autoComplete='off' />
-            
-              <br></br>
-              <br></br>
-       <label> customerNo:</label>
-         <input type="number"  onChange={handleInput} id="counter" name='customerNo' min='0' max='50'
-            autoComplete='off' />
-           
-              <br></br>
-              <br></br>
-      <label> birthday:</label>
-        <input type="date" onChange={handleInput} id="dateOfBirth" name='birthday'
-            autoComplete='off'/>
-            <br></br>
-            <br></br>
-       <label> healthInsurance:</label>
-        <input type="text"  onChange={handleInput} id="number" name='healthInsurance' />
-           <br></br>
-           <br></br>
-       <button  onClick = {handleSubmit}style={{ backgroundColor:'black' ,color:'white', marginBottom:'10px'}} type='submit'>Login</button>
-          </div>
-        </form>
-        </div>
-    </>
-  );
-}
+                          <br></br>
+                          
+                        <label> customerMobile:
+                        <input
+                            type="tel"
+                            onChange={handleInput}
+                            id="mobileNo"
+                            name="customerMobile"
+                            value={inputdata.customerMobile}
+                            autoComplete="off"
+                            placeholder="enter your mobileNumber"
+                        />
+                        </label>
+                          <br></br>
+                           
+                        <label> customerEmail:  
+                          <input                     
+                            type="email"
+                            onChange={handleInput}
+                            id="emailId"
+                            name="customerEmail"
+                            value={inputdata.customerEmail}
+                            autoComplete="off"
+                        />
+                        </label>
+                          <br></br>
+                         
+                      <label> customerNo:
+                        <input
+                            type="number"
+                            onChange={handleInput}
+                            id="counter"
+                            name="customerNo"
+                            min="0"
+                            max="50"
+                            value={inputdata.customerNo}
+                            autoComplete="off"
+                        />
+                       </label>
+                          <br></br>
+                         
+                      <label> birthday:
+                        <input
+                            type="date"
+                            onChange={handleInput}
+                            id="dateOfBirth"
+                            name="birthday"
+                            value={inputdata.birthday}
+                            autoComplete="off"
+                        />
+                      </label>
+                        <br></br>
+                        
+                        <label> healthInsurance:
+                        <input
+                            type="text"
+                            onChange={handleInput}
+                            id="number"
+                            name="healthInsurance"
+                            value={inputdata.healthInsurance}
+                        />
+                      </label>
+                        <br></br>
+                       
+                        <button
+                            onClick={handleSubmit}
+                            style={{
+                                backgroundColor: "black",
+                                color: "white",
+                                marginBottom: "10px",
+                            }}
+                            type="submit"
+                        >
+                            Save
+                        </button>
+                    </div>
+                </form>
+            </div>
+         </div>
+        </>
+    );
+};
 export default Form;
